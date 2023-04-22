5052b42a7faa1c8491dc3b54873e3606f8cbd1bc
chalitha-pramod <chalithadissanayake07@gmail.com>
2023-04-22 13:02:17 +0530

    new
    new

--- js/image-puzzle.js
+++ js/image-puzzle.js
@@ -1,91 +1,55 @@
-﻿var timerFunction;
-
-var imagePuzzle = {
-    stepCount: 0,
-    startTime: new Date().getTime(),
-    startGame: function (images, gridSize) {
-        this.setImage(images, gridSize);
-        $('#playPanel').show();
-        $('#sortable').randomize();
-        this.enableSwapping('#sortable li');
-        this.stepCount = 0;
-        this.startTime = new Date().getTime();
-        this.tick();
-    },
-    tick: function () {
-        var now = new Date().getTime();
-        var elapsedTime = parseInt((now - imagePuzzle.startTime) / 1000, 10);
-        $('#timerPanel').text(elapsedTime);
-        timerFunction = setTimeout(imagePuzzle.tick, 1000);
-    },
-    enableSwapping: function (elem) {
-        $(elem).draggable({
-            snap: '#droppable',
-            snapMode: 'outer',
-            revert: "invalid",
-            helper: "clone"
-        });
-        $(elem).droppable({
-            drop: function (event, ui) {
-                var $dragElem = $(ui.draggable).clone().replaceAll(this);
-                $(this).replaceAll(ui.draggable);
-
-                currentList = $('#sortable > li').map(function (i, el) { return $(el).attr('data-value'); });
-                if (isSorted(currentList))
-                    $('#actualImageBox').empty().html($('#gameOver').html());
-                else {
-                    var now = new Date().getTime();
-                    imagePuzzle.stepCount++;
-                    $('.stepCount').text(imagePuzzle.stepCount);
-                    $('.timeCount').text(parseInt((now - imagePuzzle.startTime) / 1000, 10));
-                }
-
-                imagePuzzle.enableSwapping(this);
-                imagePuzzle.enableSwapping($dragElem);
-            }
-        });
-    },
-
-    setImage: function (images, gridSize) {
-        console.log(gridSize);
-        gridSize = gridSize || 4; // If gridSize is null or not passed, default it as 4.
-        console.log(gridSize);
-        var percentage = 100 / (gridSize - 1);
-        var image = images[Math.floor(Math.random() * images.length)];
-        $('#imgTitle').html(image.title);
-        $('#actualImage').attr('src', image.src);
-        $('#sortable').empty();
-        for (var i = 0; i < gridSize * gridSize; i++) {
-            var xpos = (percentage * (i % gridSize)) + '%';
-            var ypos = (percentage * Math.floor(i / gridSize)) + '%';
-            var li = $('<li class="item" data-value="' + (i) + '"></li>').css({
-                'background-image': 'url(' + image.src + ')',
-                'background-size': (gridSize * 100) + '%',
-                'background-position': xpos + ' ' + ypos,
-                'width': 400 / gridSize,
-                'height': 400 / gridSize
-            });
-            $('#sortable').append(li);
-        }
-        $('#sortable').randomize();
+﻿
+function swapTiles(cell1,cell2) {
+    var temp = document.getElementById(cell1).className;
+    document.getElementById(cell1).className = document.getElementById(cell2).className;
+    document.getElementById(cell2).className = temp;
+  }
+  
+  function shuffle() {
+  //Use nested loops to access each cell of the 3x3 grid
+  for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
+     for (var column=1;column<=3;column++) { //For each column in this row
+    
+      var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
+      var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
+       
+      swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
+    } 
+  } 
+  }
+  
+  function clickTile(row,column) {
+    var cell = document.getElementById("cell"+row+column);
+    var tile = cell.className;
+    if (tile!="tile9") { 
+         //Checking if white tile on the right
+         if (column<3) {
+           if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
+             swapTiles("cell"+row+column,"cell"+row+(column+1));
+             return;
+           }
+         }
+         //Checking if white tile on the left
+         if (column>1) {
+           if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
+             swapTiles("cell"+row+column,"cell"+row+(column-1));
+             return;
+           }
+         }
+           //Checking if white tile is above
+         if (row>1) {
+           if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
+             swapTiles("cell"+row+column,"cell"+(row-1)+column);
+             return;
+           }
+         }
+         //Checking if white tile is below
+         if (row<3) {
+           if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
+             swapTiles("cell"+row+column,"cell"+(row+1)+column);
+             return;
+           }
+         } 
     }
-};
-
-function isSorted(arr) {
-    for (var i = 0; i < arr.length - 1; i++) {
-        if (arr[i] != i)
-            return false;
-    }
-    return true;
-}
-$.fn.randomize = function (selector) {
-    var $elems = selector ? $(this).find(selector) : $(this).children(),
-        $parents = $elems.parent();
-
-    $parents.each(function () {
-        $(this).children(selector).sort(function () {
-            return Math.round(Math.random()) - 0.5;
-        }).remove().appendTo(this);
-    });
-    return this;
-};
+    
+  }
